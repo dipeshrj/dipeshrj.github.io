c873a3af8d1bcef5bf58446106adc064f9894370
Dipesh Rajbanshi <deepeshraj056@gmail.com>
2023-12-24 20:07:38 +0545

    update image source

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -18,19 +18,19 @@ const Profile = () => {
 
       <div className="profile_image">
         <div className="overlay-circle">
-          <img src="src\assets\circle.svg" alt="overlay-circle" />
+          <img src="homePath\src\assets\circle.svg" alt="overlay-circle" />
         </div>
         <div className="profile-img">
-          <img src="src\assets\profile.png" alt="profile" />
+          <img src="homePath\src\assets\profile.png" alt="profile" />
         </div>
       </div>
 
       <div className="profile-icons">
         <a href="https://github.com/dipeshrj">
-          <img src="src\assets\github.png" alt="github" height={40} />
+          <img src="homePath\src\assets\github.png" alt="github" height={40} />
         </a>
         <a href="https://www.linkedin.com/in/dipeshraj056">
-          <img src="src\assets\linkedin.png" alt="linkedin" />
+          <img src="homePath\src\assets\linkedin.png" alt="linkedin" />
         </a>
       </div>
     </div>
