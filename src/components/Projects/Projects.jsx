6c96324fc01c3ac938b879ff46dd62d31d5d4a23
Dipesh Rajbanshi <deepeshraj056@gmail.com>
2024-01-07 22:18:17 +0545

    mui button

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import "./Projects.css";
+import { Button } from "@mui/material";
 
 const Projects = () => {
   return (
     <div className="projects">
       {/* jobsewa */}
       <div className="jobsewa">
-        <img
-          src="src\assets\jobsewa.png"
-          alt="jobsewa"
-          height={200}
-          width={400}
-        />
+        <img src="assets/jobsewa.png" alt="jobsewa" height={200} width={400} />
         <div className="jobsewa-details">
           <h3>Jobsewa</h3>
           <p>A job portal </p>
-          <button>
-            <a href="https://github.com/dipeshrj/Job-Portal">Soure Code</a>
-          </button>
+          <Button variant="contained">
+            <a id="anchor" href="https://github.com/dipeshrj/Job-Portal">
+              Soure Code
+            </a>
+          </Button>
         </div>
       </div>
+
       {/* e-commerce */}
       <div className="nepmart">
         <img
-          src="src\assets\e-commerce.png"
+          src="assets/e-commerce.png"
           alt="e-commerce"
           height={200}
           width={400}
@@ -31,17 +30,21 @@ const Projects = () => {
         <div className="nepmart-details">
           <h3>Nepmart</h3>
           <p>An e-commerce website </p>
-          <button>
-            <a href="https://github.com/dipeshrj/JavaScript-Project-E-commerce-">
-              Soure Code
+          <Button variant="contained">
+            <a
+              id="anchor"
+              href="https://github.com/dipeshrj/JavaScript-Project-E-commerce-"
+            >
+              Source Code
             </a>
-          </button>
+          </Button>
         </div>
       </div>
+
       {/* todoapp */}
       <div className="todoapp">
         <img
-          src="src\assets\todoapp.png"
+          src="assets/todoapp.png"
           alt="to-do-app"
           height={200}
           width={400}
@@ -49,9 +52,11 @@ const Projects = () => {
         <div className="todoapp-details">
           <h3>ToDo App</h3>
           <p>A todo web-app </p>
-          <button>
-            <a href="https://github.com/dipeshrj/To-Do-App">Soure Code</a>
-          </button>
+          <Button variant="contained">
+            <a id="anchor" href="https://github.com/dipeshrj/To-Do-App">
+              Soure Code
+            </a>
+          </Button>
         </div>
       </div>
     </div>
